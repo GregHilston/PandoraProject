80e5492bfc09713dc40507c2f67ff6b79fa452c4
Greg Hilston <Grehgh@gmail.com>
2014-07-22 21:10:21 -0400

    Initial commit. Script does pull all thumbed up songs to consoule. Takes a very long time and definately a memory issue. Possibly with 'setTimeout'. Memory usage pushed 1.3 GBs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 PandoraProject
 ==============
 
-Interfacing with Pandora
+As there is no official API for Pandora and the unofficial API hasn't been working as expected, I'll be writing scripts to grab the data I need from Pandora.
---
+++ get_thumbed_up_songs.js
@@ -0,0 +1,32 @@
+number_of_thumbed_up_songs = 713;
+max = Math.ceil(number_of_thumbed_up_songs / 5); // Pandora shows thumbed up songs in units of 5 songs per click of "show more"
+counter = 0;
+
+console.log("Total Iterations: " + max);
+
+var showMore = function(){
+	$('.backstage .show_more').trigger('click');
+	$("html, body").animate({ scrollTop: $(document).height() }, "fast");
+	
+	console.log("Iterations Complete: " + counter + " / " + max);
+	counter++;
+	
+	if(counter < max) {
+		setTimeout(showMore, 1500); // Check again
+	}
+	else {
+		console.log("Grabbing Tracks!");
+		$tracks = $('div#track_like_pages div.section');
+
+		for (var i = 0; i < $tracks.length; i++) {
+			var t = $tracks.eq(i);
+			var title = $.trim($('h3.normal', t).text());
+			var artist = $.trim($('div.infobox-body p.s-0 a', t).eq(0).text());
+			console.log(title + ' - ' + artist);
+		 }
+		 
+		 alert(number_of_thumbed_up_songs + " tracks recorded");
+	}
+}
+
+showMore()

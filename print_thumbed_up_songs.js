f134bb3eb83196a2fde08ab2edb975a37bbc754b
Greg Hilston <Grehgh@gmail.com>
2014-07-31 23:48:15 -0400

    Delete print_thumbed_up_songs.js

--- print_thumbed_up_songs.js
+++
@@ -1,8 +0,0 @@
-$tracks = $('div#track_like_pages div.section');
-
-for (var i = 0; i < $tracks.length; i++) {
-	var t = $tracks.eq(i);
-	var title = $.trim($('h3.normal', t).text());
-	var artist = $.trim($('div.infobox-body p.s-0 a', t).eq(0).text());
-	console.log(title + ' - ' + artist);
-}
